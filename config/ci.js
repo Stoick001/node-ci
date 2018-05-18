4e1b58244b749726e88738e38e0d857f641e26ea
Gordan <gordan.prastalo.gp@gmail.com>
2018-05-18 12:12:47 +0200

    Travis setup

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://admin:testpass123@ds219000.mlab.com:19000/nodeadvanced',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379',
 };
